345862b5a17a36f89b57dd45751898a7787c423d
William Mallett <MallettWilliam@JohnDeere.com>
2019-04-27 10:32:22 -0500

    adding src dir

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const routes = require('./src/routes');
 const app = express();
 const helmet = require('helmet');
 const config = require('./config');
